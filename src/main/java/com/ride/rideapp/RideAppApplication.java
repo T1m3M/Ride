8cf08a1bc4fc6dee7a6dd459ed0ef71ac680a71a
T1m3-m4ch1n3 <abdelrahman_nasr99@hotmail.com>
2022-01-02 09:01:44 +0200

    adding test login service in main

--- src/main/java/com/ride/rideapp/RideAppApplication.java
+++ src/main/java/com/ride/rideapp/RideAppApplication.java
@@ -1,13 +1,29 @@
 package com.ride.rideapp;
 
+import com.ride.rideapp.login.LoginRepo;
+import com.ride.rideapp.login.LoginService;
+import com.ride.rideapp.models.Admin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RideAppApplication {
 
+    private static LoginService login;
+
+    public RideAppApplication(LoginService login) {
+        RideAppApplication.login = login;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(RideAppApplication.class, args);
+
+        Admin test = login.loginAdmin("123", "123");
+
+        System.out.println(test.getId());
+        System.out.println(test.getUsername());
+        System.out.println(test.getMobile());
+        System.out.println(test.getPassword());
     }
 
 }
