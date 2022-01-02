6439f594239a64f2f8d8e01d356fc305eb46f786
T1m3-m4ch1n3 <abdelrahman_nasr99@hotmail.com>
2022-01-02 17:12:02 +0200

    adding event class

---
+++ src/main/java/com/ride/rideapp/models/Event.java
@@ -0,0 +1,4 @@
+package com.ride.rideapp.models;
+
+public class Event {
+}
--- src/main/java/com/ride/rideapp/showOffer/ShowOfferController.java
+++ src/main/java/com/ride/rideapp/showOffer/ShowOfferController.java
@@ -1,7 +1,9 @@
 package com.ride.rideapp.showOffer;
 
+import com.ride.rideapp.models.Event;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,7 +13,7 @@ public class ShowOfferController {
     ShowOfferService showOfferService;
 
     @GetMapping("/show_offer")
-    public Event showOffer(@RequestMapping("ride_id") Integer ride_id) {
+    public Event showOffer(@RequestParam("ride_id") Integer ride_id) {
         return null;
     }
 
