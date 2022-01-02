d138333c03e2f3b32c253b609c9ac0adf7004b40
T1m3-m4ch1n3 <abdelrahman_nasr99@hotmail.com>
2022-01-02 09:39:11 +0200

    adding ride structure

---
+++ src/main/java/com/ride/rideapp/models/Ride.java
@@ -0,0 +1,73 @@
+package com.ride.rideapp.models;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.stereotype.Component;
+
+import java.sql.Timestamp;
+
+@Component
+public class Ride {
+    @Id
+    int id;
+
+    String source, destination;
+    Timestamp start_time, end_time;
+    int offer_id;
+    boolean ride_status;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public Timestamp getStart_time() {
+        return start_time;
+    }
+
+    public void setStart_time(Timestamp start_time) {
+        this.start_time = start_time;
+    }
+
+    public Timestamp getEnd_time() {
+        return end_time;
+    }
+
+    public void setEnd_time(Timestamp end_time) {
+        this.end_time = end_time;
+    }
+
+    public int getOffer_id() {
+        return offer_id;
+    }
+
+    public void setOffer_id(int offer_id) {
+        this.offer_id = offer_id;
+    }
+
+    public boolean getRide_status() {
+        return ride_status;
+    }
+
+    public void setRide_status(boolean ride_status) {
+        this.ride_status = ride_status;
+    }
+}
