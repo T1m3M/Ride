be6d1733bcbc10a2fee2ffee80efa8d0ab464088
T1m3-m4ch1n3 <abdelrahman_nasr99@hotmail.com>
2022-01-03 14:11:43 +0200

    refactoring accept offer to when only customer can pay

--- src/main/java/com/ride/rideapp/acceptOffer/AcceptOfferRepo.java
+++ src/main/java/com/ride/rideapp/acceptOffer/AcceptOfferRepo.java
@@ -1,6 +1,8 @@
 package com.ride.rideapp.acceptOffer;
 
+import com.ride.rideapp.mappers.CustomerRowMapper;
 import com.ride.rideapp.mappers.OfferRowMapper;
+import com.ride.rideapp.models.Customer;
 import com.ride.rideapp.models.Offer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -23,17 +25,30 @@ public class AcceptOfferRepo {
 
             // updating offer object
             Offer offer = result.get(0);
-            offer.setOffer_status(false);
-            offer.setAccept_time(new Timestamp(System.currentTimeMillis()));
 
-            sql = "UPDATE offers SET offer_status=?, accept_time=? WHERE id=" + offer_id;
-            conn.update(sql, offer.getOffer_status(), offer.getAccept_time());
+            // getting customer balance
+            float fare = offer.getAfter_discount();
+            int ride_id = offer.getRide_id();
 
-            // change ride status
-            sql = "UPDATE rides SET offer_id=?, ride_status=? WHERE id=" + offer.getRide_id();
-            conn.update(sql, offer.getId(), false);
+            sql = "SELECT customers.* FROM customers, rides WHERE rides.id=" + ride_id + " AND rides.customer_id = customers.id";
+            List<Customer> customers = conn.query(sql, new CustomerRowMapper());
 
-            return offer;
+            float customer_balance = customers.get(0).getBalance();
+
+            // only when the customer can pay
+            if (customer_balance >= fare) {
+                offer.setOffer_status(false);
+                offer.setAccept_time(new Timestamp(System.currentTimeMillis()));
+
+                sql = "UPDATE offers SET offer_status=?, accept_time=? WHERE id=" + offer_id;
+                conn.update(sql, offer.getOffer_status(), offer.getAccept_time());
+
+                // change ride status
+                sql = "UPDATE rides SET offer_id=?, ride_status=? WHERE id=" + offer.getRide_id();
+                conn.update(sql, offer.getId(), false);
+
+                return offer;
+            }
         }
 
         return null;
